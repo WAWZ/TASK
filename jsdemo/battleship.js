e3d1b5ebf4b1970dca5de23647ee8be505263441
WAWZ <2522135713@qq.com>
2018-06-23 10:35:34 +0800

    w

--- jsdemo/battleship.js
+++ jsdemo/battleship.js
@@ -1,30 +1,46 @@
 "use static";
-
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSank = false;
-while (isSank == false){
-    guess = prompt ("Ready,aim,fire!(enter a number form 0-6);");
-    if (guess <0 || guess > 6){
-        alert("Please enter a valid cell number!");
-    }else{
-        guesses += 1;
-        if(guess ==location1 || guess == location2 || guess == location3){
-            alert("Hits!");
-            hits += 1;
-            if(hits == 3){
-                isSank = true;
-                alert("You sank my ship!");
-            }
-        }else{
-            alert("Miss!");
-        }
-    }
+// var randomLoc = Math.floor(Math.random()*5);
+// var location1 = randomLoc;
+// var location2 = randomLoc + 1;
+// var location3 = randomLoc + 2;
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+// var isSank = false;
+// while (isSank == false){
+//     guess = prompt ("Ready,aim,fire!(enter a number form 0-6);");
+//     if (guess <0 || guess > 6){
+//         alert("Please enter a valid cell number!");
+//     }else{
+//         guesses += 1;
+//         if(guess ==location1 || guess == location2 || guess == location3){
+//             alert("Hits!");
+//             hits += 1;
+//             if(hits == 3){
+//                 isSank = true;
+//                 alert("You sank my ship!");
+//             }
+//         }else{
+//             alert("Miss!");
+//         }
+//     }
+// }
+// var stats = "You took"+ guesses + "guesses to sank the battleship," +
+//     "which means your shooting accuracy was "+ (3/guesses);
+// alert(stats);
+function doIt(param) {
+    param = 2;
 }
-var stats = "You took"+ guesses + "guesses to sank the battleship," +
-    "which means your shooting accuracy was "+ (3/guesses);
-alert(stats);
+var test = 1;
+doIt(test);
+// console.log(test);
+alert(test);
+function
+changeStuff(num){
+    num = num * 10;
+    return num;
+}
+var num = 10;
+var n = changeStuff(num);
+// alert(num);//10
+alert(n);
